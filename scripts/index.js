a90d27d4a8b509d4c2113c37b7c01fc208aab840
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-06-12 16:04:40 +0500

    Correct the working of the typewriter. Now it working not bad

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,35 @@
-'use strict'
-
 function typeWriter() {
-    let text = `Hello! This is a translater — site where you can translate Russian or English text to "Morse", "Binary", "Decimal" and "Hex" codes.`;
-    let speed = 250;
-    let paragraphArea = document.getElementById('typewriter');
+    const textArea = document.getElementById("typewriter"); //get area for text
+    let text = [
+        "Hello! ",
+        "This is a site, ",
+        "where you can translate ",
+        "from Russian or English, ",
+        "to Morse, Binary, Decimal and Hex ",
+        "languages! Welcome!",
+    ]; //our text
+    let lineCounter = 0; //line from array
+    let symbolCounter = 0; //symbol from array
+    let oneSymbol = "";
 
-    function typeWriterText() {
-        let i = 0;
-        if (i < text.length) {
-            paragraphArea.textContent += text.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
+    function typeWriteText() {
+        //draw the line
+        let interval = setTimeout(function() {
+            oneSymbol += text[lineCounter][symbolCounter];
+            textArea.textContent = oneSymbol + "|"; //write symbol + "|"
+            symbolCounter++; //increase symbol counter by one point
+            if (symbolCounter >= text[lineCounter].length) {
+                symbolCounter = 0; //reset the symbol counter
+                lineCounter++; //increase line counter by one point
+                if (lineCounter == text.length) {
+                    clearTimeout(interval); //stop the timeout
+                    textArea.textContent = oneSymbol; //remove "|"
+                    return true; //stop the function working
+                }
+            }
+            typeWriteText(); //start the function in the interval
+        }, 300);
     }
-    typeWriterText();
+    typeWriteText(); //start the function for start all with interval
 }
 typeWriter();
