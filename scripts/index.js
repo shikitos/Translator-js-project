a4c3b0e36d93dd4a6236ff228d44d2b0a51d30d6
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-06-13 16:32:33 +0500

    Fix typewriter. Now it works with the blinking cursor (but need to correct it

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,36 @@
 function typeWriter() {
     const textArea = document.getElementById("typewriter"); //get area for text
+    let verticalLine = document.querySelector('#vertical-line'); //create an element span
+    // verticalLine.setAttribute('id', 'vertical-line', '|'); //give it id=#vertical-line
+    // let insertLine = textArea.appendChild(verticalLine); //insert span at the end of the paragraph text
     let text = [
         "Hello! ",
-        "This is a site, ",
-        "where you can translate ",
+        "Here you can translate ",
         "from Russian or English, ",
         "to Morse, Binary, Decimal and Hex ",
-        "languages! Welcome!",
+        "code. ;) ",
     ]; //our text
     let lineCounter = 0; //line from array
     let symbolCounter = 0; //symbol from array
     let oneSymbol = "";
 
+
     function typeWriteText() {
         //draw the line
         let interval = setTimeout(function() {
             oneSymbol += text[lineCounter][symbolCounter];
-            textArea.textContent = oneSymbol + "|"; //write symbol + "|"
+            textArea.innerHTML = oneSymbol; //write symbol + "|"
             symbolCounter++; //increase symbol counter by one point
             if (symbolCounter >= text[lineCounter].length) {
                 symbolCounter = 0; //reset the symbol counter
                 lineCounter++; //increase line counter by one point
                 if (lineCounter == text.length) {
                     clearTimeout(interval); //stop the timeout
-                    textArea.textContent = oneSymbol; //remove "|"
                     return true; //stop the function working
                 }
             }
             typeWriteText(); //start the function in the interval
-        }, 300);
+        }, 200);
     }
     typeWriteText(); //start the function for start all with interval
 }
