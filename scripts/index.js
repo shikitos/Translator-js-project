6c0c01b94f9a2158d9f96f14f1302017c76d3971
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-06-13 22:51:11 +0500

    Add alpabet for decode morse (international and russian). Try to make translate  function

--- scripts/index.js
+++ scripts/index.js
@@ -95,24 +95,37 @@ const morseInternational = {
     "7": "--...",
     "8": "---..",
     "9": "----.",
+    " ": "  "
 }
+
+
 
 //click on button and make translation
 function submitTranslate() {
     let textAreaFrom = document.getElementById('text-from');
     let buttonTranslate = document.querySelector('.translate');
     buttonTranslate.addEventListener('click', event => {
+        // function takeValueByOneSymbol() {
         //take one symbol from each word
+        let arr = [];
+        let text;
         let textValue = textAreaFrom.value.split();
         for (let i = 0; i < textValue.length; i++) {
             for (let j = 0; j < textValue[i].length; j++) {
-                console.log(textValue[i][j]);
+                arr += textValue[i][j];
+
             }
         }
         for (let prop in morseInternational) {
-            if (textValue == prop) {
-                let text = morseInternational[textValue];
+            console.log('out');
+            let arrayOfMorseKeys = prop;
+            if (arr == prop) {
+                console.log('here');
+
+                text += morseInternational[arr];
                 console.log(text);
+            } else {
+                return text;
             }
         }
 
@@ -120,3 +133,102 @@ function submitTranslate() {
 }
 
 submitTranslate();
+
+decodeMorse = function(morseCode, lang) {
+    const morseInternational = {
+        '.-': 'A',
+        '-...': 'B',
+        '-.-.': 'C',
+        '-..': 'D',
+        '.': 'E',
+        '..-.': 'F',
+        '--.': 'G',
+        '....': 'H',
+        '..': 'I',
+        '.---': 'J',
+        '-.-': 'K',
+        '.-..': 'L',
+        '--': 'M',
+        '-.': 'N',
+        '---': 'O',
+        '.--.': 'P',
+        '--.-': 'Q',
+        '.-.': 'R',
+        '...': 'S',
+        '-': 'T',
+        '..-': 'U',
+        '...-': 'V',
+        '.--': 'W',
+        '-..-': 'X',
+        '-.--': 'Y',
+        '--..': 'Z',
+        '...---...': 'SOS',
+        '--..--': '!',
+        '..--..': '?',
+        '......': '.',
+        '.-.-.-': ',',
+        '---...': ':',
+        '-.-.-.': ';',
+        '.----': '1',
+        '..---': '2',
+        '...--': '3',
+        '....-': '4',
+        '.....': '5',
+        '-....': '6',
+        '--...': '7',
+        '---..': '8',
+        '----.': '9',
+        '-----': '0'
+    };
+
+    const morseRu = {
+        '.-': 'А',
+        '-...': 'Б',
+        '-.-.': "Ц",
+        '-..': "Д",
+        '.': "Е",
+        '..-.': "Ф",
+        '--.': "Г",
+        '....': "Х",
+        '..': "И",
+        '.---': "Й",
+        '-.-': "К",
+        '.-..': "Л",
+        '--': "М",
+        '-.': "Н",
+        '---': "О",
+        '.--.': "П",
+        '--.-': "Щ",
+        '.-.': "Р",
+        '...': "С",
+        '-': "Т",
+        '..-': "У",
+        '...-': "Ж",
+        '.--': "В",
+        '-..-': "Ь",
+        '-.--': "Ы",
+        '--..': "З",
+        '---.': "Ч",
+        '----': "Ш",
+        '--.--': "Ъ",
+        '..-..': "Э",
+        '..--': "Ю",
+        '.-.-': "Я",
+        '--..--': '!',
+        '..--..': '?',
+        '......': '.',
+        '.-.-.-': ',',
+        '---...': ':',
+        '-.-.-.': ';',
+        '.----': '1',
+        '..---': '2',
+        '...--': '3',
+        '....-': '4',
+        '.....': '5',
+        '-....': '6',
+        '--...': '7',
+        '---..': '8',
+        '----.': '9',
+        '-----': '0'
+    };
+}
