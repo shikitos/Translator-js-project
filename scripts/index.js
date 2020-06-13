3a3b3b98914cba5e218bbbad64efcaa312a4fc48
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-06-13 18:04:59 +0500

    Add the International morse alphabet. Start to create a function of the translation from English to morse.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//Create typeWriter at the header of the page
 function typeWriter() {
     const textArea = document.getElementById("typewriter"); //get area for text
     let verticalLine = document.querySelector('#vertical-line'); //create an element span
@@ -7,7 +8,7 @@ function typeWriter() {
         "Hello! ",
         "Here you can translate ",
         "from Russian or English ",
-        "to Morse, Binary ....",
+        "to Morse, Binary, Hex ",
         "codes. ;) ",
     ]; //our text
     let lineCounter = 0; //line from array
@@ -30,8 +31,92 @@ function typeWriter() {
                 }
             }
             typeWriteText(); //start the function in the interval
-        }, 200);
+        }, randomNumber(100, 400));
+
+        //Create random number for the interval (for making typeWriter more humanity)
+        function randomNumber(min, max) {
+            let random = min + Math.random() * (max + 1 - min);
+            return Math.floor(random);
+        }
     }
     typeWriteText(); //start the function for start all with interval
 }
 typeWriter();
+
+//morse alphabet
+const morseInternational = {
+    "a": ".-",
+    // "A": ".-",
+    "b": "-...",
+    // 'B': '-...',
+    "c": "-.-.",
+    // 'C': '-.-.',
+    "d": "-..",
+    // 'D': '-..',
+    "e": ".",
+    // 'E': '.',
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "!": "-.-.--",
+    "-": "-....-",
+    "/": "-..-.",
+    "@": ".--.-.",
+    "(": "-.--.",
+    ")": "-.--.-",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+}
+
+//click on button and make translation
+function submitTranslate() {
+    let textAreaFrom = document.getElementById('text-from');
+    let buttonTranslate = document.querySelector('.translate');
+    buttonTranslate.addEventListener('click', event => {
+        //take one symbol from each word
+        let textValue = textAreaFrom.value.split();
+        for (let i = 0; i < textValue.length; i++) {
+            for (let j = 0; j < textValue[i].length; j++) {
+                console.log(textValue[i][j]);
+            }
+        }
+        for (let prop in morseInternational) {
+            if (textValue == prop) {
+                let text = morseInternational[textValue];
+                console.log(text);
+            }
+        }
+
+    });
+}
+
+submitTranslate();
